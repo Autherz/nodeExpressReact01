d31a51e086143a03440dbcf73c5381c055b3723d
Autherz <atisit_thongbai@kkumail.com>
2018-07-18 00:15:12 +0700

    'addTopLevelApi'

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('babel-register');
+require('./src/js/server.js');
+
 var app = express();
 
 // view engine setup
